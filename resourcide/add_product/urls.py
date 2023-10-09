37382febbb6788b74bca1fd833283ae13657e954
ptmp25 <68309978+ptmp25@users.noreply.github.com>
2023-10-10 01:16:24 +0700

    Update urls.py

--- resourcide/add_product/urls.py
+++ resourcide/add_product/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('add_product/step1/', views.first_step, name='first_step'),
     path('add_product/step2/', views.second_step, name='second_step'),
     path('add_product/<int:product_id>/step3/', views.third_step, name='third_step'),
-    path('add_product/step4/', views.fourth_step, name='fourth_step')
+    path('add_product/<int:product_id>/step4/', views.fourth_step, name='fourth_step')
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
